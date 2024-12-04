053777fc14a2658e11eac66b7c57e036492e5f4d
Aleksandr Zakharov <ZamArcher@gmail.com>
2024-12-04 22:21:48 +0500

    Add a new module settings.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 
 class AlienInvasion:
     """Класс для управления ресурсами и поведением игры."""
@@ -10,8 +12,11 @@ class AlienInvasion:
         """Инициализирует игру и создает игровые ресурсы."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
         pygame.display.set_caption('Alien Invasion')
 
         # задание цвета фона.
@@ -26,7 +31,7 @@ class AlienInvasion:
                     sys.exit()
 
             # При каждом проходе цикла перерисовывается экран.
-            self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
 
             # Отображение последнего прорисованного экрана.
             pygame.display.flip()
